62447af816ed16338581c556e0a1ba418ea27713
Monkhsaikhan123 <98628666+Monkhsaikhan123@users.noreply.github.com>
2024-03-19 02:58:53 +0800

    Add files via upload

---
+++ src/components/Payment.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar1 from './Navbar1'
+import Footer from './Footer'
+
+
+const Payment = () => {
+  return (
+    
+    <div>
+        <Navbar1/>
+        <div className='max-w-screen-2x1 container flexmx-auto xl:px-24 px-4 py-28 '>
+            
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Payment
